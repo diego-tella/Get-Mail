3a1c73554b07d69bc00d68a585d94beb156a0ede
Diego Tellaroli <diegotellaroli05@gmail.com>
2021-10-29 20:02:52 -0300

    Update Program.cs
    aaaaaaa

--- Get Mail/Get Mail/Program.cs
+++ Get Mail/Get Mail/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -23,34 +23,38 @@ namespace Get_Mail
             Console.WriteLine(@"\    \_\  \  ___/|  |   /    Y    \/ __ \|  |  |__");
             Console.WriteLine(@" \______  /\___  >__|   \____|__  (____  /__|____/");
             Console.WriteLine(@"        \/     \/               \/     \/         ");
+            Console.WriteLine("------https://github.com/diego-tella/Get-Mail------\n");
         }
         public static bool FileOrURL()
         {
             volt1:
             banner();
-            Console.WriteLine("Qual o nome do site ou o arquivo com os sistes que você deseja extrair os emails?");
+            Console.WriteLine("Pass a site or a list (.txt file) with different sites to start the scan");
             StrFileorUrl = Console.ReadLine();
             if (!StrFileorUrl.Contains("http") && !StrFileorUrl.Contains(".txt"))
             {
-                Console.WriteLine("Você precisa passar um arquivo ou uma URL.");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You need to pass a valid URL or a text file");
                 Thread.Sleep(3000);
+                Console.ResetColor();
                 Console.Clear();
                 goto volt1;
             }
             else
             {
                 if (StrFileorUrl.Contains("http"))
-                    return false;
+                    return false; //return false case it is a site
                 else
-                    return true; //file
+                    return true;
             }
         }
         public static void GetMails()
         {
-            if(FileOrURL()) //arquivo | ele já executa aqui ao invés de voltar uma variável
+            if(FileOrURL()) 
             {
                 StreamReader str = new StreamReader(StrFileorUrl);
-                StreamWriter esc = new StreamWriter("Encontrados.txt");
+                StreamWriter esc = new StreamWriter("found.txt");
+                esc.WriteLine("List of all emails found");
                 string str2;
                 while((str2 = str.ReadLine()) != null)
                 {
@@ -60,7 +64,7 @@ namespace Get_Mail
                     {
                         try
                         {
-                            data = web.DownloadString(str2); //pega o código da página
+                            data = web.DownloadString(str2);
                         }
                         catch(Exception ex)
                         {
@@ -70,59 +74,68 @@ namespace Get_Mail
 
                     if (Reg.IsMatch(data))
                     {
-                        MatchCollection matches = Reg.Matches(data); //coloca em uma lista "Match Coleção" todos os emails
-                        string[] strvalor = new string[matches.Count]; //cria um array do tipo string com o tamanho da lista de Match Coleção
+                        MatchCollection matches = Reg.Matches(data);
+                        string[] strvalor = new string[matches.Count]; 
                         int j = 0;
-                        foreach (Match math in matches) //Cria um elemento Match na "Match coleção"
+                        foreach (Match math in matches) 
                         {
-                            strvalor[j] = math.ToString(); //passa o valor do math 1 para o array de strings
+                            strvalor[j] = math.ToString(); 
                             j++;
                         }
-                        string[] q = strvalor.Distinct().ToArray(); //cria um novo array com os valores de strvalor sem os valores repetidos
-                        Console.WriteLine("\n-----------------------------------------------------------------------\n" + "Emails encontrados em: " + str2);
-                        esc.WriteLine("\n-----------------------------------------------------------------------\n" + "Emails encontrados em: " + str2);
-                        foreach (var math in q) //foreach no novo array criado
+                        string[] q = strvalor.Distinct().ToArray();
+                        Console.WriteLine("\n-----------------------------------------------------------------------\n" + "Emails found in: " + str2);
+                        foreach (var math in q) 
                         {
-                            Console.WriteLine(math); //printa o array
+                            Console.WriteLine(math); 
                             esc.WriteLine(math);
                         }
                     }
                     else
                     {
-                        Console.WriteLine("\n-----------------------------------------------------------------------\n" + "Emails encontrados em: " + str2);
-                        esc.WriteLine("\n-----------------------------------------------------------------------\n" + "Emails encontrados em: " + str2);
-                        Console.WriteLine("Não foi achado nenhum email :/");
-                        esc.WriteLine("Não foi achado nenhum email :/");
+                        Console.WriteLine("\n-----------------------------------------------------------------------\n" + "Emails found in: " + str2);
+                        Console.WriteLine("No email found :/ ");
 ;                    }
                 }
                 esc.Close();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nA list with all emails found was created in " + Environment.CurrentDirectory + @"\found.txt");
+                Console.ResetColor();
             }
             else //url
             {
                 Regex Reg = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", RegexOptions.IgnoreCase); //[A-Za-z0-9_\-\+]+@[A-Za-z0-9\-]+\.([A-Za-z]{2,3})(?:\.[a-z]{2})?
                 string data;
-                using (WebClient web = new WebClient())
-                    data = web.DownloadString(StrFileorUrl); //pega o código da página
+                try
+                {
+                    using (WebClient web = new WebClient())
+                        data = web.DownloadString(StrFileorUrl);
+                }
+                catch(Exception e)
+                {
+                    data = "";
+                    Console.WriteLine("Error! " + e.ToString());
+                    Environment.Exit(0);
+                }
 
                 if (Reg.IsMatch(data))
                 {
-                    MatchCollection matches = Reg.Matches(data); //coloca em uma lista "Match Coleção" todos os emails
-                    string[] strvalor = new string[matches.Count]; //cria um array do tipo string com o tamanho da lista de Match Coleção
+                    MatchCollection matches = Reg.Matches(data); 
+                    string[] strvalor = new string[matches.Count]; 
                     int j = 0;
-                    foreach (Match math in matches) //Cria um elemento Match na "Match coleção"
+                    foreach (Match math in matches) 
                     {
-                        strvalor[j] = math.ToString(); //passa o valor do math 1 para o array de strings
+                        strvalor[j] = math.ToString(); 
                         j++;
                     }
-                    string[] q = strvalor.Distinct().ToArray(); //cria um novo array com os valores de strvalor sem os valores repetidos
-                    Console.WriteLine("\n"+ q.Length +" emails encontrados!");
-                    foreach (var math in q) //foreach no novo array criado
-                        Console.WriteLine(math); //printa o array
+                    string[] q = strvalor.Distinct().ToArray(); 
+                    Console.WriteLine("\n"+ q.Length + " emails found!");
+                    foreach (var math in q) 
+                        Console.WriteLine(math);
                 }
                 else
-                    Console.WriteLine("Não foi achado nenhum email :/");
+                    Console.WriteLine("No email found :/");
             }
-            Console.WriteLine("\n----------------------------------------------------------------------\nFim do scan. Pressione qualquer tecla pra sair.");
+            Console.WriteLine("\n----------------------------------------------------------------------\nEnd of scan. press any key to exit");
             Console.ReadKey();
         }
     }
